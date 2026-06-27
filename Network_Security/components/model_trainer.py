749ca3cc7d306ade81bcb286ace83cee6fda28e6
Krrish-Dangi <krrishdangi2005@gmail.com>
2026-06-27 22:34:03 +0530

    pipelines implemented

--- Network_Security/components/model_trainer.py
+++ Network_Security/components/model_trainer.py
@@ -25,6 +25,9 @@ from sklearn.ensemble import (
 )
 import mlflow
 
+import dagshub
+dagshub.init(repo_owner='Krrish-Dangi', repo_name='End-to-End-MLOps-Project', mlflow=True)
+
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
         try:
